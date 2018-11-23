4e265d477a1831c07edf07f0f539f637ea46b140
Leo Mancini <mancini@leo.gd>
2018-11-23 14:22:56 -0800

    Fixing paths

--- views/shared/header.php
+++ views/shared/header.php
@@ -3,11 +3,11 @@
 	
 	if($_SERVER['SERVER_NAME'] == "localhost" || $_SERVER['SERVER_NAME'] == "leo.local") {
 		$server = "local";
+		$path = "/portfolio2018/git/";
 	} else {
 		$server = "remote";
+		$path = "/portfolio2018/";
 	}
-	
-	$path = "/portfolio2018/no-content/";
 	
 	$browsers_with_stylesheet_variation = Array("chrome", "safari");
 	
