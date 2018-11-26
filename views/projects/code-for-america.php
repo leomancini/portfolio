f826254192e5fdddf69329b17cd4958fa5896237
Leo Mancini <mancini@leo.gd>
2018-11-25 20:58:01 -0800

    Tweaking language on CFA projects

--- views/projects/code-for-america.php
+++ views/projects/code-for-america.php
@@ -17,12 +17,12 @@
 			<div class="text">
 			<div class="text-block">
 				<span class="title">Problem</span>
-				<p>America’s public workforce system is a set of government services designed to help people find and keep high-quality jobs, including job search assistance, referrals to training programs, and connections to employers.</p><p>The delivery of these services is often shaped by policies that were written without a good understanding of the people using these services. Job seekers are asked to navigate complicated systems, use unintuitive interfaces, and struggle with bureaucratic procedures.</p></div><div class="text-block">
+				<p>America’s public workforce system is a set of government services designed to help people find and keep high-quality jobs.</p><p>The delivery of these services is frequently shaped by policies written without a good understanding how people use these services. Job seekers are often asked to navigate complicated systems, use unintuitive interfaces, and struggle with bureaucratic procedures.</p></div><div class="text-block">
 				<span class="title">Solution</span>
-				<p>Working with policy and product managers, we conducted research to identify systemic issues in the workforce system in several states. We interviewed dozens of case managers and job seekers to understand the problems they’re facing.</p><p>We prototyped and tested various technology products to help people find and keep high-quality jobs. We used these experiments to inspire government partners to use technology to improve delivery of workforce services. 
+				<p>We conducted research to identify systemic issues in the workforce system. We interviewed dozens of case managers and job seekers.</p><p>We prototyped and tested various technology products to help people find and keep high-quality jobs. We used these experiments to inspire government partners to use technology to improve delivery of workforce services. 
 </p></div><div class="text-block">
 				<span class="title">Role</span>
-				<p>Collaborating with a small cross-functional team, I led research, design, prototyping, and experimentation.</p><p>I designed high-fidelity screens, mocked up interactive demos, and <a target="_blank" href="https://github.com/topics/codeforamerica-workforce-2018">built out functional prototypes and websites</a>.</p><p>We conducted user research sessions and analyzed usage metrics to test our ideas. We gathered feedback from government leaders and non-profits to refine and improve our products.</p></div>
+				<p>Collaborating with a small cross-functional team, I led research, design, prototyping, and experimentation.</p><p>I designed high-fidelity screens, mocked up interactive demos, and <a target="_blank" href="https://github.com/topics/codeforamerica-workforce-2018">built out functional prototypes and websites</a>.</p><p>We gathered feedback from government leaders and non-profits to refine and improve our products.</p></div>
 			</div>
 			<div class="links">
 				<a target="_blank" href="https://github.com/topics/codeforamerica-workforce-2018">Prototype Code</a><a target="_blank" href="https://www.google.org/our-work/economic-opportunity/code-for-america/">Grant from Google.org</a><a target="_blank" href="https://www.doleta.gov/business/pws.cfm">Overview of Public Workforce System</a>
@@ -52,7 +52,7 @@
 						<img src="<?php echo $path; ?>content/<?php echo $project; ?>/thumbs/fair-chance.jpg" style="width: 168px; height: 114px;">
 					</div>
 				</div>
-				<label>Conviction History Appeal</label>
+				<label>Appeal Job Rejection</label>
 			</div>
 			<div class="slide-thumbnail-wrapper" id="slide_3">
 				<div class="slide-thumbnail">
@@ -60,7 +60,7 @@
 						<img src="<?php echo $path; ?>content/<?php echo $project; ?>/thumbs/kupana.jpg" style="width: 168px; height: 114px;">
 					</div>
 				</div>
-				<label>Caregiver Financial Aid</label>
+				<label>Pay for Caregiving Costs</label>
 			</div>
 			<div class="slide-thumbnail-wrapper" id="slide_4">
 				<div class="slide-thumbnail">
@@ -68,7 +68,7 @@
 						<img src="<?php echo $path; ?>content/<?php echo $project; ?>/thumbs/corner.jpg" style="width: 168px; height: 114px;">
 					</div>
 				</div>
-				<label>Emotional Support</label>
+				<label>Get Emotional Support</label>
 			</div>
 		</div>
 	</div>
