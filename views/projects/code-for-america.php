48ad7378f3ebe2417beb935564591035be03ac11
Leo Mancini <mancini@leo.gd>
2018-12-05 21:21:25 -0800

    Updating Code for America subtitle

--- views/projects/code-for-america.php
+++ views/projects/code-for-america.php
@@ -4,7 +4,7 @@
 <div id="<?php echo $project; ?>-content" class="section light white monochrome">
 	<div class="header">
 		<h1>Code for America</h1>
-		<h2>Find and maintain high-quality jobs and training.</h2>
+		<h2>Find a job that pays enough to live on.</h2>
 	</div>
 
 	<button id="info-button" class="expand">
