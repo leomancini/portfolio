e568af12a9a612bd45e1fb91338d373fc9d2c62b
Leo Mancini <mancini@leo.gd>
2025-11-25 22:18:20 -0500

    Updating title

--- views/resume.php
+++ views/resume.php
@@ -96,7 +96,7 @@
 	</div>
 	<div id="position">
 		<h3><a href="https://leomancini.net/##fb-fte" target="_blank" rel="noopener">Facebook</a></h3>
-		<h4>Lead Product Designer &nbsp;<span class="inline-description">October 2012 &ndash; September 2017</span></h4>
+		<h4>Staff Product Designer &nbsp;<span class="inline-description">October 2012 &ndash; September 2017</span></h4>
 		<div class="description">
 			<p>Collaborated with product teams to identify high-impact problems and develop solutions in sharing, audience, and public content</p>
 			<ul>
